0e6304478d0e473ad57a61b333e57784f56f57bc
Olesya <lesusha1@yandex.ru>
2023-03-07 19:57:32 +0300

    changed Cat

--- game/Cat.java
+++ game/Cat.java
@@ -1,7 +1,7 @@
 public class Cat extends BaseParticipant {
 
     public Cat(String name, int jump, int run) {
-        super(name, jump, run, -1);
+        super(name, jump, run, -1, -1);
     }
 
 }
