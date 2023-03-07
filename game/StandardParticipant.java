94c43c570ea2442abd14e04985125786884b306f
Olesya <lesusha1@yandex.ru>
2023-03-07 20:00:36 +0300

    changed StandardParticipant

--- game/StandardParticipant.java
+++ game/StandardParticipant.java
@@ -1,7 +1,7 @@
 public class StandardParticipant extends BaseParticipant {
 
-    public StandardParticipant(String name, int jump, int run, int swim) {
-        super(name, jump, run, swim);
+    public StandardParticipant(String name, int jump, int run, int swim, int bicycle) {
+        super(name, jump, run, swim, bicycle);
     }
 
 }
